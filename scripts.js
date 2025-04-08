80d7e996411c53a6736df604706a5571f4bf3dd0
necro549 <tobias.gemmeke@yahoo.com>
2025-04-08 17:06:11 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,29 +4,45 @@ document.addEventListener('DOMContentLoaded', function () {
     const livePreview = document.getElementById('live-preview');
 
     const fonts = [
-        'https://drive.google.com/uc?export=view&id=12itQA30oq7Pdk217ma-WmCpwfNGFlGb2',
-        'https://drive.google.com/uc?export=view&id=1AC-LqdJmyW-fN6bUwNL2poE_2kp4vMXA',
-        'https://drive.google.com/uc?export=view&id=1AIWhpyfZUyWJ8dQRhxM7r_jvVbq_R93y',
-        'https://drive.google.com/uc?export=view&id=1Uo5hQFpkoODoPYKqoWuo8dVzCJKpcGCJ',
-        'https://drive.google.com/uc?export=view&id=1VFLe-kNg2_MVlnNX56s9sSiF44xVMhvP',
-        'https://drive.google.com/uc?export=view&id=1X8NiGuoicSduxEroVfMPiTkIsSPkVQAy',
-        'https://drive.google.com/uc?export=view&id=1Xk2q8D0K9G6i7Om7wGsVKrSqtkPREq4d',
-        'https://drive.google.com/uc?export=view&id=1aHlWaEqQRI3Ap05YXhiqkbbBk6oEE9Gv',
-        'https://drive.google.com/uc?export=view&id=1aZeHAIjkYrRYnKCZPYB8b3OdUcZllqRD',
-        'https://drive.google.com/uc?export=view&id=1ep904OgXNv_sGulqRhYkBT56OhyPqc1N',
-        'https://drive.google.com/uc?export=view&id=1mukkNjWoOkzCaTkrIJTgKHoxtispe0H-',
-        'https://drive.google.com/uc?export=view&id=1nTCwrafhELh7a9XhTDYqU_QoKGdtklaV',
-        'https://drive.google.com/uc?export=view&id=1rGSqxvOQYt89cUDTujC-qhDY3BnjWCIm',
-        'https://drive.google.com/uc?export=view&id=1tZ8njTyN4ZYEZETwiYKYsv5pXHOPBLgR'
+        'Schriftart1',
+        'Schriftart2',
+        'Schriftart3',
+        'Schriftart4',
+        'Schriftart5',
+        'Schriftart6',
+        'Schriftart7',
+        'Schriftart8',
+        'Schriftart9',
+        'Schriftart10',
+        'Schriftart11',
+        'Schriftart12',
+        'Schriftart13',
+        'Schriftart14'
+    ];
+
+    const fontUrls = [
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%201.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%202.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%203.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%204.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%205.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%206.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%207.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%208.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%209.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%2010.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%2011.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%2012.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%2013.woff2',
+        'https://github.com/necro549/Selfmadeplott-/raw/d61cf98eec3d94ea4ed7977b0601599337fca467/Schriftnr%2014.woff2'
     ];
 
     // Load fonts dynamically
     fonts.forEach((font, index) => {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = font;
-        link.id = `font${index + 1}`;
-        document.head.appendChild(link);
+        const fontFace = new FontFace(font, `url('${fontUrls[index]}')`);
+        fontFace.load().then(function(loadedFontFace) {
+            document.fonts.add(loadedFontFace);
+        });
     });
 
     textInput.addEventListener('input', updateLivePreview);
